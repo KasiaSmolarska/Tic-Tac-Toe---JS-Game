9ba6dff325a6a15042b8c74dae63d6f1de873db3
KasiaSmolarska <kokasmok7@gmail.com>
2018-11-03 16:37:43 +0100

    appearance improvements

--- src/class.tile.js
+++ src/class.tile.js
@@ -20,10 +20,14 @@ class Tile {
     setValue(value){
         if(this.value === undefined){
             this.value = value;
+            const x = `<svg viewBox="0 0 52 52">  <path class="cross__path cross__path--right" fill="none" d="M16,16 l20,20" />  <path class="cross__path cross__path--left" fill="none" d="M16,36 l20,-20" /> </svg>`;
+            const o = `<svg viewBox="0 0 24 24"> <path fill="#000000" d="M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" /> </svg>`;
             if (value === 'x') {
                 this.li.classList.add('tile--x');
+                this.li.innerHTML = x;
             } else if( value === 'y'){
-                this.li.classList.add('tile--y');
+                this.li.classList.add('tile--o');
+                this.li.innerHTML = o;
             } else{
                 console.warn('zła wartość, podałaś: ', value);
             }
